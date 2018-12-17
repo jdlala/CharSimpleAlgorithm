2ee5c36676da76829007782aedeeea73fe6d5502
liao <ao.li@jsxfedu.com>
2018-12-17 16:13:16 +0800

    将数组转换成字符串的工具，用于打印数组显示

---
+++ src/Utils.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Utils {
+    //将数组转换成字符串打印出来
+    public String array(List a) {
+        String array = "[";
+        for (int i = 0;i < a.size();i++) {
+            if (i != a.size() - 1) {
+                array += a.get(i) + ",";
+            } else {
+                array += a.get(i);
+            }
+        }
+        array += "]";
+        return array;
+    }
+
+    //将数组转换成字符串打印出来
+    public String intArray(int [] a) {
+        String array = "[";
+        for (int i = 0;i < a.length;i++) {
+            if (i != a.length - 1) {
+                array += a[i] + ",";
+            } else {
+                array += a[i];
+            }
+        }
+        array += "]";
+        return array;
+    }
+}
